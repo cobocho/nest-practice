40a3d9ddb5cec397f8fe77ee0fb4e14b5ac9562f
cobocho <rlaalsrb1111@naver.com>
2024-03-18 23:44:11 +0900

    section05

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,7 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { PostModel } from 'src/types/Post';
 
-const posts: PostModel[] = [
+let posts: PostModel[] = [
   {
     id: 1,
     author: 'newjeans_official',
@@ -14,7 +23,15 @@ const posts: PostModel[] = [
     id: 2,
     author: 'newjeans_official',
     title: '뉴진스 해린',
-    content: '메이크업 고치는 해린',
+    content: '노래 연습하는 해린',
+    likeCount: 10000,
+    commentCount: 9999,
+  },
+  {
+    id: 3,
+    author: 'blackpink_official',
+    title: '블랙핑크 로제',
+    content: '춤추는 로제',
     likeCount: 10000,
     commentCount: 9999,
   },
@@ -23,7 +40,71 @@ const posts: PostModel[] = [
 @Controller('posts')
 export class PostsController {
   @Get()
-  getPost(): PostModel[] {
+  getPosts(): PostModel[] {
     return posts;
+  }
+
+  @Get(':id')
+  getPost(@Param('id') id: string) {
+    const post = posts.find((post) => post.id === +id);
+
+    if (!post) {
+      throw new NotFoundException();
+    }
+
+    return post;
+  }
+
+  @Post()
+  postPost(
+    @Body('author') author: string,
+    @Body('title') title: string,
+    @Body('content') content: string,
+  ) {
+    const post: PostModel = {
+      id: posts.at(-1).id + 1,
+      author,
+      title,
+      content,
+      likeCount: 0,
+      commentCount: 0,
+    };
+
+    posts.push(post);
+
+    return post;
+  }
+
+  @Patch(':id')
+  patchPost(
+    @Param('id') id: string,
+    @Body('author') author: string,
+    @Body('title') title: string,
+    @Body('content') content: string,
+  ) {
+    const post = posts.find((post) => post.id === +id);
+
+    if (!post) {
+      throw new NotFoundException();
+    }
+
+    if (author) {
+      post.author = author;
+    }
+
+    if (title) {
+      post.title = title;
+    }
+
+    if (content) {
+      post.content = content;
+    }
+
+    return post;
+  }
+
+  @Delete(':id')
+  deletePost(@Param('id') id: string) {
+    posts = posts.filter((post) => post.id !== +id);
   }
 }
